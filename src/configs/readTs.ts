48281aa3794b96cbc6f7b959e58139dfe65e675c
heifade <heifade@126.com>
2018-10-21 13:28:33 +0800

    1.0.4

--- src/configs/readTs.ts
+++ src/configs/readTs.ts
@@ -1,22 +1,28 @@
-import { resolve as resolvePath, dirname, } from "path";
+import { resolve as resolvePath } from "path";
 import { unlinkSync, existsSync } from "fs";
 import { spawn } from "child_process";
 
+function getTsc() {
+  const path1 = resolvePath(__dirname, "../node_modules/typescript/bin/tsc"); // 打包后 __dirname 变成 dist， 所以是'..'
+  if (existsSync(path1)) {
+    return path1;
+  }
+  const path2 = resolvePath(process.cwd(), "./node_modules/typescript/bin/tsc");
+  if (existsSync(path2)) {
+    return path2;
+  }
+  throw new Error("tsc is not exists!");
+}
+
 export async function readTs(file: string) {
   return new Promise<any>((resolve, reject) => {
-    let jsFileName = file.replace(/.ts$/, ".js");
+    const jsFileName = file.replace(/.ts$/, ".js");
 
-    const typescript = require.resolve("typescript");
-    const tsc = resolvePath(dirname(typescript), "../bin/tsc");
-
-    if (!existsSync(tsc)) {
-      throw new Error("tsc is not exists!");
-    }
     if (existsSync(jsFileName)) {
       unlinkSync(jsFileName);
     }
 
-    const client = spawn(tsc, [file, "--module", "commonjs"], { shell: true });
+    const client = spawn(getTsc(), [file, "--module", "commonjs"], { shell: true });
 
     client.on("close", (code, signal) => {
       if (existsSync(jsFileName)) {
